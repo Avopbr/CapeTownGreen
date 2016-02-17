56f73530cbf2292f100b0445e98ac1642451bb74
Avopbr <abadwanguzeli@gmail.com>
2016-02-17 10:35:46 +0200

    submitting my keycode

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
-
-var body = document.querySelector("body");
+/*var body = document.querySelector("body");
 
 body.onkeydown = function(e){
     //e.keyCode - will capture the key codes
+   //displayMessage(e.keyCode);*/
+
+var keyCodeName = function(keycode) {
+	if(keycode === 39) {
+		return "right";
+	   }
+	if(keycode === 37) {
+		return "left";
+	   }
+	if(keycode === 38) {
+		return "up";
+	   }
+	if(keycode === 40) {
+		return "down";
+	   }
+	if(keycode === 56||32||64) {
+	    return "";
+	   }
 };
