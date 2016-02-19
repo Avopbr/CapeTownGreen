69a12e13531d21b7ca89cf3ab7419738e45679c4
Avopbr <abadwanguzeli@gmail.com>
2016-02-19 14:51:33 +0200

    submitting createLocationClass

--- app.js
+++ app.js
@@ -6,39 +6,71 @@ var body = document.querySelector("body");
 body.onkeydown = function(e){
 
 //     e.keyCode - will capture the key codes
-      var KeyNames = keyCodeName(e.keyCode);
+      var KeyNames = keyCodeName(e.num);
+      	// var locationClass = createLocationClass(e.num);
  			displayMessage(taxiLocationCounter);
+ 				moveForward();
 };
 
-		var taxiLocationCounter = 0;
-			taxiLocationCounter++;
+		var taxiLocationCounter = 1;
+			taxiLocationCounter++;	
+
 
 
 var keyCodeName = function(keycode) {
 	if(keycode === 39) {
 		return "right";
 	   }
-	if(keycode === 37) {
-		return "left";
-	   }
-	if(keycode === 38) {
-		return "up";
-	   }
-	if(keycode === 40) {
-		return "down";
-	   }
-	else {
-	    return "";
-	   }
-
+		if(keycode === 37) {
+			return "left";
+	   		}
+			if(keycode === 38) {
+				return "up";
+	   			}
+				if(keycode === 40) {
+					return "down";
+	   				}
+					else {
+	    				return "";
+	   						}
 };
 
 function createLocationClass(num) {
 if (num === 1) {
 	return "slot-one-of-nine";
 }
-
+	 else if (num === 2) {
+		return "slot-two-of-nine";
+	}
+	   else if (num === 3) {
+			return "slot-three-of-nine";
+		}
+			else if (num === 4) {
+				return "slot-four-of-nine";
+			}
+				else if (num === 5) {
+					return "slot-five-of-nine";
+				}
+					else if (num === 6) {
+						return "slot-six-of-nine";
+					}
+						else if (num === 7) {
+							return "slot-seven-of-nine";
+						}
+							else if (num === 8) {
+								return "slot-eight-of-nine";
+							}
+								else if (num === 9) {
+									return "slot-nine-of-nine";
+									}
 };
+
+function moveForward() {
+	var currentLocation = createLocationClass(taxiLocationCounter);
+		var newLocation = createLocationClass(taxiLocationCounter);
+			moveTaxi(currentLocation, newLocation);
+}
+
 
 
 
